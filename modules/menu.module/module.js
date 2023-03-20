50f203444fc00a903e5be4bf425fa3bc05c31d7b
Saar Speksnijder <Saarconversioncrew>
2023-03-20 17:04:38 +0100

    adjusting module and section to bootstrap 5

--- modules/menu.module/module.js
+++ modules/menu.module/module.js
@@ -1,72 +1,40 @@
-// Website header variables
+document.addEventListener("DOMContentLoaded", function(){
 
-var menuParentItems = document.querySelectorAll('.menu--desktop .menu__item--has-submenu');
-var childToggle = document.querySelectorAll('.menu--mobile .menu__child-toggle');
-
-// Desktop menu
-
-if (menuParentItems) {
-  Array.prototype.forEach.call(menuParentItems, function(el){
-
-    // Menu item variables
-
-    var childToggle = el.querySelector('.menu__child-toggle');
-
-    // Handles hover over
-
-    el.addEventListener('mouseover', function(){
-      this.classList.add('menu__item--open');
-      this.querySelector('a').setAttribute('aria-expanded', 'true');
-      this.querySelector('button').setAttribute('aria-expanded', 'true');
+  /////// Prevent closing from click inside dropdown
+  document.querySelectorAll('.dropdown-menu').forEach(function(element){
+    element.addEventListener('click', function (e) {
+      e.stopPropagation();
     });
-
-    // Handles hover out
-
-    el.addEventListener('mouseout', function(){
-      document.querySelector('.menu__item--open > a').setAttribute('aria-expanded', 'false');
-      document.querySelector('.menu__item--open > button').setAttribute('aria-expanded', 'false');
-      document.querySelector('.menu__item--open').classList.remove('menu__item--open');
-    });
-
-    // Handles toggle of submenus
-
-    childToggle.addEventListener('click', function(){
-      if (this.parentNode.classList.contains('menu__item--open')) {
-        this.parentNode.classList.remove('menu__item--open');
-        this.parentNode.querySelector('a').setAttribute('aria-expanded', 'false');
-        this.parentNode.querySelector('button').setAttribute('aria-expanded', 'false');
-      }
-      else {
-        this.parentNode.classList.add('menu__item--open');
-        this.parentNode.querySelector('a').setAttribute('aria-expanded', 'true');
-        this.parentNode.querySelector('button').setAttribute('aria-expanded', 'true');
-      }
-    });
-
   });
 
-}
-
-// Mobile menu
-
-// Handles toggle of submenus
-
-if (childToggle) {
-  Array.prototype.forEach.call(childToggle, function(el){
-
-    el.addEventListener('click', function(){
-      this.classList.toggle('menu__child-toggle--open');
-      if (this.parentNode.classList.contains('menu__item--open')) {
-        this.parentNode.classList.remove('menu__item--open');
-        this.parentNode.querySelector('a').setAttribute('aria-expanded', 'false');
-        this.parentNode.querySelector('button').setAttribute('aria-expanded', 'false');
-      }
-      else {
-        this.parentNode.classList.add('menu__item--open');
-        this.parentNode.querySelector('a').setAttribute('aria-expanded', 'true');
-        this.parentNode.querySelector('button').setAttribute('aria-expanded', 'true');
-      }
+   // make it as accordion for smaller screens
+  if (window.innerWidth < 992) {
+    // close all inner dropdowns when parent is closed
+    document.querySelectorAll('.navbar .dropdown').forEach(function(everydropdown){
+      everydropdown.addEventListener('hidden.bs.dropdown', function () {
+        // after dropdown is hidden, then find all submenus
+          this.querySelectorAll('.megasubmenu').forEach(function(everysubmenu){
+            // hide every submenu as well
+            everysubmenu.style.display = 'none';
+          });
+      })
     });
 
-  });
-}
+    document.querySelectorAll('.has-submenu a').forEach(function(element){
+      element.addEventListener('click', function (e) {
+          let nextEl = this.nextElementSibling;
+          if(nextEl && nextEl.classList.contains('megasubmenu')) {	
+            // prevent opening link if link needs to open dropdown
+            e.preventDefault();
+            if(nextEl.style.display == 'block'){
+              nextEl.style.display = 'none';
+            } else {
+              nextEl.style.display = 'block';
+            }
+          }
+      });
+    }) // end foreach
+  }
+  // end if innerWidth
+}); 
+// DOMContentLoaded  end
